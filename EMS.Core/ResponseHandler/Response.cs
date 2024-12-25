d353c19adbe11760ff668662381a511c286cdfae
Ahm3dSala5 <yarapharpyaa55@gmail.com>
2024-12-25 17:50:21 +0200

    ResponseHandler

---
+++ EMS.Core/ResponseHandler/Response.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+namespace EMS.Core.ResponseHandler
+{
+    public class Response<T>
+    {
+        public Response()
+        {
+
+        }
+
+        public Response(T _data, string _message = null)
+        {
+            this.Succeeded = true;
+            this.Data = _data;
+            this.Message = _message;
+        }
+
+        public Response(string _message)
+        {
+            this.Succeeded = false;
+            this.Message = _message;
+        }
+
+        public Response(string _message,bool _succeeded)
+        {
+            this.Succeeded = _succeeded;
+            this.Message = _message;
+        }
+        
+        public HttpStatusCode Status { set; get; }
+        public T Data { set; get; }
+        public object Meta { set; get; }
+        public List<string> Errors { set; get; }
+        public bool Succeeded { set; get; }
+        public string Message { set; get; }
+    }
+}
---
+++ EMS.Core/ResponseHandler/ResponseHandler.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+
+namespace EMS.Core.ResponseHandler
+{
+    public class ResponseHandler
+    {
+        public Response<T> Create<T>(T _data ,object meta = null)
+        {
+            return new Response<T>()
+            {
+                Data = _data,
+                Succeeded = true,
+                Status = HttpStatusCode.Created ,
+                Message = "Created"
+            };
+        }
+
+        public Response<T> Delete<T>(string _message = null)
+        {
+            return new Response<T>()
+            {
+                Succeeded = true,
+                Status = HttpStatusCode.OK,
+                Message = _message is null ? "Deleted" : _message
+            };
+        }
+
+        public Response<T> Success<T>(T _data,object _meta = null)
+        {
+            return new Response<T>()
+            {
+                Succeeded = true,
+                Status = HttpStatusCode.OK,
+                Data = _data,
+                Meta = _meta
+            };
+        }
+
+        public Response<T> Unauhorized<T>(string _message = null)
+        {
+            return new Response<T>()
+            {
+                Succeeded = true,
+                Status = HttpStatusCode.Unauthorized,
+                Message = _message
+            };
+        }
+
+        public Response<T> BadRequest<T>(string _message = null)
+        {
+            return new Response<T>()
+            {
+                Succeeded = false,
+                Status = HttpStatusCode.BadRequest,
+                Message = _message is null ? "BadRequest" : _message
+            };
+        }
+
+        public Response<T> UnprocessableEntity<T>(string _message = null)
+        {
+            return new Response<T>()
+            {
+                Succeeded = false,
+                Status = HttpStatusCode.UnprocessableEntity,
+                Message = _message is null ? "UnprocessableEntity" : _message
+            };
+        }
+
+        public Response<T> NotFound<T>(string _message = null)
+        {
+            return new Response<T>()
+            {
+                Succeeded = false,
+                Status = HttpStatusCode.NotFound,
+                Message = _message is null ? "NotFound" : _message
+            };
+        }
+    }
+}
